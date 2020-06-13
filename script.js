cc2a21725d659ac669252f70d89338a3f9691b52
Julia <jugo839@users.noreply.github.com>
2020-06-13 17:28:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,7 @@
-//document.getElementById("rating_PCA_routes").style.display = "block";
-
-
-/*
-$(function() {
-    
-    // listen for changes
-    $('input[type="radio"]').on('change', function(){
-        
-        // get checked one            
-        var $target = $('input[type="radio"]:checked');
-        // hide all divs with .showhide class
-        $(".showhide").hide();
-        // show div that corresponds to selected radio.
-        $( $target.attr('data-section') ).show();
-
-    // trigger the change on page load
-    }).trigger('change');
-
-});
-
-
-function showHide(element_id){
-	//Если элемент с id-шником element_id существует
-	if (document.getElementById(element_id)){
-	//Записываем ссылку на элемент в переменную obj
-		var obj = document.getElementById(element_id); 
-		//Если css-свойство display не block, то: 
-		if (obj.style.display != "block"){ 
-			obj.style.display = "block"; //Показываем элемент
-		}else{
-			obj.style.display = "none"; //Скрываем элемент
-			}
-	}
-	//Если элемент с id-шником element_id не найден, то выводим сообщение
-	else{
-		alert("Элемент с id: " + element_id + " не найден!");
-	} 
-}   
-function showhidediv( rad )
-    {
-        var rads = document.getElementsByName( rad.name );
-        document.getElementById( 'rating_DEA_routes' ).style.display = ( rads["dea"].checked ) ? 'block' : 'none';
-        document.getElementById( 'rating_PCA_routes' ).style.display = ( rads["pca"].checked ) ? 'block' : 'none';
-    }
-	
-	
-	
-function showDiv2(select){
-		   if(select.checked=='dea' && select.value=='routes'){
-			document.getElementById('rating_DEA_routes').style.display = "block";
-			document.getElementById('rating_DEA_regions').style.display = "none";
-			document.getElementById('rating_DEA_postcodes').style.display = "none";
-			document.getElementById('rating_PCA_routes').style.display = "none";
-			document.getElementById('rating_PCA_regions').style.display = "none";
-			document.getElementById('rating_PCA_postcodes').style.display = "none";
-		   }else if(select.checked=='dea' && select.value=='regions'){
-			document.getElementById('rating_DEA_regions').style.display = "block";
-			document.getElementById('rating_DEA_routes').style.display = "none";
-			document.getElementById('rating_DEA_postcodes').style.display = "none";
-			document.getElementById('rating_PCA_routes').style.display = "none";
-			document.getElementById('rating_PCA_regions').style.display = "none";
-			document.getElementById('rating_PCA_postcodes').style.display = "none";
-		   }
-		} 
-
-
-*/
+/* ----------------- Arrow Up -------------------*/		
+window.onscroll = function arrowUp() {
+  document.getElementById('arrowUp').style.display = (window.pageYOffset > '200' ? 'block' : 'none');
+}
 
 function showDiv(select){
 		  if(select.value=='null'){
@@ -121,27 +57,4 @@ function showDiv(select){
 		   }
 			
 			
-			/*else{
-			document.getElementById('hidden_div').style.display = "none";
-		   }*/
 		} 
-/* ----------------- For Parents/Coaches -------------------*/		
-/*function showHide(element_id){
-	//Если элемент с id-шником element_id существует
-	//if (document.getElementById(element_id)){
-	document.getElementsByClassName(element_id){
-	//Записываем ссылку на элемент в переменную obj
-		//var obj = document.getElementById(element_id); 
-		var obj = document.getElementsByClassName(element_id); 
-		//Если css-свойство display не block, то: 
-		if (obj.style.display != "block"){ 
-			obj.style.display = "block"; //Показываем элемент
-		}else{
-			obj.style.display = "none"; //Скрываем элемент
-			}
-	}
-	//Если элемент с id-шником element_id не найден, то выводим сообщение
-	else{
-		alert("Элемент с id: " + element_id + " не найден!");
-	} 
-}   */
